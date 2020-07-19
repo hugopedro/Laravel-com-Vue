f471b869a0bf42cc4d081a52944b3928696afce0
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2020-07-19 16:41:15 -0300

    17 e 18 Criação de um campo de Busca

--- blog/resources/assets/js/components/TabelaLista.vue
+++ blog/resources/assets/js/components/TabelaLista.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <a v-if="criar" v-bind:href="criar">Criar</a>
+    <div class="form-inline">
+      <a v-if="criar" v-bind:href="criar">Criar</a>
+      <div class="form-group pull right"></div> <!-- pull right coloca o elemento à direita-->
+      <input type="search" class="form-control" placeholder="Buscar" v-model="buscar" >
+    </div>
 
     <table class="table table-striped table-hover">
       <thead>
@@ -11,7 +15,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item,index) in itens">
+        <tr v-for="(item,index) in lista">
           <td v-for="i in item">{{i}}</td>
 
           <td v-if="detalhe || editar || deletar">
@@ -51,9 +55,28 @@
 <script>
     export default {
       props:['titulos','itens','criar','detalhe','editar','deletar','token'],
+      data: function() {
+        return {
+          buscar:''
+        }
+      },
       methods:{
         executaForm: function(index){
           document.getElementById(index).submit();
+        }
+      },
+      computed: {
+        lista:function() {
+          let busca = "php";
+          return this.itens.filter(res => {
+            for (let k = 0; k < res.length; k++) {
+              if ((res[k] + "").toLowerCase().indexOf(this.buscar.toLowerCase()) >= 0 ) {
+                return true;
+              } //((res[k] + "") transforma inteiro para string. aqui é a função buscar
+            }
+            return false;
+          });
+          return this.itens;
         }
       }
     }
