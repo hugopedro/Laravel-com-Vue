e94ad5e2b7c43f6c4c4939d5e38ec35c0d79b7de
Hugo Pedro <hugopedro098@gmail.com>
2020-08-06 15:47:08 -0300

    25 Criando componente de modal

--- blog/resources/views/admin/artigos/index.blade.php
+++ blog/resources/views/admin/artigos/index.blade.php
@@ -4,10 +4,14 @@
   <pagina tamanho="12">
     <painel titulo="Lista de Artigos">
       <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
+
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#meuModalTeste">Large Model</button>
+
+
       <tabela-lista
       v-bind:titulos="['#','Título','Descrição']"
       v-bind:itens="[[1,'PHP OO','Curso de PHP OO'],[2,'Vue JS','Curso de Vue JS']]"
-      ordem="asc" ordemcol="2"
+      ordem="desc" ordemcol="1"
       criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="7887522323"
 
 
@@ -16,4 +20,31 @@
     </painel>
 
   </pagina>
+
+  <modal nome="meuModalTeste">
+    <painel titulo="Adicionar">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputFile">File input</label>
+        <input type="file" id="exampleInputFile">
+        <p class="help-block">Example block-level help text here.</p>
+      </div>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Check me out
+        </label>
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    </painel>
+
+  </modal>
 @endsection
